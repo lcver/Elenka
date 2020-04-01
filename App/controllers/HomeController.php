da79cb30cf540806f14e328127e958535b222a0a
lcver <crackover1092@gmail.com>
2020-04-02 01:01:32 +0700

    view mapel siswa in home page

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -9,19 +9,66 @@ class HomeController extends Controller
             self::auth();
             return false;
         }
-        $data['page'] = "Home";
-        $this->view('Home/index', $data);
+
+            $result = $this->model('PaketSoalModel')->show('siswa');
+            if(!is_null($result)){
+                $key = array_keys($result);
+    
+                $count = count($key);
+                $num = NULL;
+    
+                for ($i=0; $i < $count ; $i++) { 
+                    if(is_numeric($key[$i])) $num = true;
+                }
+    
+                // foreach ($resultkey as $key) {
+                //     if(!is_numeric($key)) $num = false;
+                // }
+                    if(!$num):
+                        $data[] = $result;
+                    else:
+                        $data = $result;
+                    endif;
+                // var_dump($data);
+                // die();
+            }else{
+                $data=NULL;
+            }
+
+            $this->view('home/index', $data);
     }
 
 
     public function auth()
     {
-        $this->view('Auth/index');
+        if(!isset($_SESSION['elenka_usersession'])){
+            $this->view('Auth/index');
+        }
     }
 
     public function authentication()
     {
-        var_dump($_POST);
+        $account = [
+            'username'=>$_POST['elenka_username'],
+            'password'=>$_POST['elenka_password']
+        ];
+
+        if ($res=$this->model('SiswaModel')->auth($account))
+        {   
+            /**
+             * set session
+             */
+            $_SESSION['elenka_usersession'] = $res['id'];
+            $_SESSION['elenka_username'] = $res['nama'];
+            $_SESSION['elenka_userkelas'] = $res['idKelas'];
+            $_SESSION['elenka_userbagian'] = $res['idBagian'];
+
+        }else
+        {
+            Flasher::setFlash('Username dan password tidak valid', false);
+        }
+
+        header('location:'.BASEURL);
     }
 }
 
