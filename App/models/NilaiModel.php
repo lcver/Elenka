f577c5118ca9a519c6f8d85ef6a6eec03ab7f5d2
lcver <crackover1092@gmail.com>
2020-04-02 02:12:25 +0700

    create condition Nilai models different Admin

--- App/models/NilaiModel.php
+++ App/models/NilaiModel.php
@@ -13,7 +13,28 @@ class NilaiModel extends Controller
      * 
      * create view resource data
      */
-    public function create(){}
+    public function create(){
+        return Database::table('tbelenka_nilai_siswa')
+                                    ->join('tbelenka_siswa')
+                                    ->on('tbelenka_nilai_siswa.idSiswa','tbelenka_siswa.id')
+                                    ->join('tbelenka_kelas')
+                                    ->on('tbelenka_siswa.idKelas','tbelenka_kelas.id')
+                                    ->join('tbelenka_kelas_bagian')
+                                    ->on('tbelenka_siswa.idBagian','tbelenka_kelas_bagian.id')
+                                    ->join('tbelenka_paket_soal')
+                                    ->on('tbelenka_paket_soal.idGuru = '.$_SESSION['elenka_adminsession'].' and tbelenka_nilai_siswa.idPaketSoal','tbelenka_paket_soal.id')
+                                    ->join('tbelenka_matapelajaran')
+                                    ->on('tbelenka_paket_soal.idMatapelajaran','tbelenka_matapelajaran.id')
+                                    ->fetch([
+                                        'tbelenka_matapelajaran.id as idMatapelajaran',
+                                        'tbelenka_matapelajaran.pelajaran',
+                                        'tbelenka_siswa.nama',
+                                        'tbelenka_nilai_siswa.nilai',
+                                        'tbelenka_kelas.kelas',
+                                        'tbelenka_kelas_bagian.bagian'
+                                        ])
+                                    ->get();
+    }
         /**
          * 
          * stored new resourece data
