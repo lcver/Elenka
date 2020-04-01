d6e075b50556a2395b3ec446c00697aec2dcc187
lcver <crackover1092@gmail.com>
2020-04-01 19:39:10 +0700

    set config

--- App/config/config.php
+++ App/config/config.php
@@ -1,9 +1,9 @@
 <?php
 // url
-define('BASEURL','http://localhost/project/Amum/public/');
+define('BASEURL','http://localhost/project/Elenka/public_html/');
 
 // db
 define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASS', '');
-define('DBNAME', 'db_amum');
+define('DBNAME', 'db_elenka');
