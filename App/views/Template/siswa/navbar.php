2a5c733c2862aa674f9df0dca0506097db381503
lcver <crackover1092@gmail.com>
2020-04-02 18:29:49 +0700

    fix position logout button

--- App/views/Template/siswa/navbar.php
+++ App/views/Template/siswa/navbar.php
@@ -14,19 +14,22 @@
 
     <div class="navbar-nav ml-auto">
         <?php if(isset($_SESSION['elenka_usersession'])) : ?>
-        <li class="nav-item">
-            <span class="nav-link text-gray-dark">             
+        <li class="nav-item dropdown">
+            <span class="nav-link text-gray-dark text-sm" data-toggle="dropdown">
                 <?= $_SESSION['elenka_username']?> 
             </span>
+            <div class="dropdown-menu dropdown-menu-md">
+                <div class="dropdown-item" data-toggle="dropdown">
+                    <a href="<?=BASEURL?>home/logout" class="btn btn-danger" data-toggle="dropdown">
+                        <i class="fas fa-power-off"></i>
+                        <span>Sign Out</span>
+                    </a>
+                </div>
+            </div>
         </li>
         <!-- <li class="nav-item"></li> -->
-        <!-- <span class="pl-2 pr-2" >|</span> -->
-        <li class="nav-item">
-            <a href="<?=BASEURL?>home/logout" class="nav-link bg-danger">
-                <i class="fas fa-power-off"></i>
-                <span>Logout</span>
-            </a>
-        </li>
+        <!-- <li class="nav-item">
+        </li> -->
         <?php endif; ?>
     </div>
 </div>
