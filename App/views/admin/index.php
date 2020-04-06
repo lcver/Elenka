36faadf0ef0c4694726ea121188e345dcde74f9b
lcver <crackover1092@gmail.com>
2020-04-06 10:45:52 +0700

    fix random nilai when bagian guru 3

--- App/views/admin/index.php
+++ App/views/admin/index.php
@@ -43,7 +43,8 @@
             </thead>
             <tbody>
             <?php //var_dump($data) ?>
-                <?php $no=0; foreach ($data as $d) : $no++; ?>
+                <?php $no=0; foreach ($data as $d) : ?>
+                <?php if($d['idBagian']==$_SESSION['elenka_adminbagian']): $no++;?>
                     <tr>
                         <td><?=$no?></td>
                         <td><?=$d['nama']?></td>
@@ -70,9 +71,36 @@
                         <?php if(isset($ips)): ?>
                             <td><?=$d['ips']?></td>
                         <?php endif; ?>
-                        
-                        
                     </tr>
+                <?php elseif ($_SESSION['elenka_adminbagian']==3): ?>
+                    <tr>
+                        <td><?=$no?></td>
+                        <td><?=$d['nama']?></td>
+                        
+                        <td><?=$d['kelas'].' - '.$d['bagian']?></td>
+                        <?php if(isset($ppkn)): ?>
+                            <td><?=$d['ppkn']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($bindo)): ?>
+                            <td><?=$d['bindo']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($matematika)): ?>
+                            <td><?=$d['matematika']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($sbdp)): ?>
+                            <td><?=$d['sbdp']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($pjok)): ?>
+                            <td><?=$d['pjok']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($ipa)): ?>
+                            <td><?=$d['ipa']?></td>
+                        <?php endif; ?>
+                        <?php if(isset($ips)): ?>
+                            <td><?=$d['ips']?></td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endif;?>
                 <?php endforeach; ?>
             </tbody>
         </table>
